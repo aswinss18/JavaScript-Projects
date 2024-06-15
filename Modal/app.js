4f35af101a40fc9ebb60f6879b86cf52a0ae0ac6
aswinss18 <aswinartography18@gmail.com>
2024-06-15 10:11:01 +0530

    Opening and closing of modal window is created

---
+++ Modal/app.js
@@ -0,0 +1,14 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+const openModalBtn = document.querySelector(".modal-btn");
+const closeModalBtn = document.querySelector(".close-btn");
+const modal = document.querySelector(".modal-overlay");
+openModalBtn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+});
+
+closeModalBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
